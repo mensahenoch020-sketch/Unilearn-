18162bb6e3962df8d7a51e643668fecc3eb92e76
mensahenoch020-sketch <mensahenoch020@gmail.com>
2026-04-02 06:29:38 +0100

    Delete src/rolewrapper.jsx

--- src/rolewrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function RoleWrapper({ currentRole, requiredRole, children }) {
-  if (currentRole === requiredRole) {
-    return <>{children}</>;
-  }
-  return null; 
-}
