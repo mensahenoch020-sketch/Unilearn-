e8177620cfdf836b6503b76fa5f7631e7d77812d
mensahenoch020-sketch <mensahenoch020@gmail.com>
2026-04-04 20:27:22 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const FACULTIES = {
+  "Communication & Information Sciences": ["Computer Science","Information Technology","Library & Information Science","Telecommunication Science"],
+  "Engineering & Technology": ["Civil Engineering","Electrical Engineering","Mechanical Engineering","Chemical Engineering"],
+  "Physical Sciences": ["Biochemistry","Chemistry","Computer Science","Mathematics","Microbiology","Physics","Statistics"],
+  "Social Sciences": ["Accounting","Banking & Finance","Business Administration","Economics","Political Science","Sociology"],
+  "Management Sciences": ["Accounting","Banking & Finance","Business Administration","Economics"],
+  "Arts": ["Arabic","English","French","History","Islamic Studies","Yoruba"],
+  "Education": ["Educational Technology","Guidance & Counselling","Science Education","Social Studies Education"],
+  "Law": ["Law"],
+  "Medicine & Health Sciences": ["Medicine","Nursing","Pharmacy","Dentistry"],
+  "Pharmaceutical Sciences": ["Pharmacy"],
+  "Life Sciences": ["Biochemistry","Microbiology","Zoology","Botany"],
+  "Agriculture": ["Agricultural Economics","Agronomy","Animal Science","Soil Science"],
+};
+
+export const LEVELS = ["100","200","300","400","500"];
+export const SEMESTERS = ["Harmattan","Rain"];
